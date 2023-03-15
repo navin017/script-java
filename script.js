afe7ff173caf1478954fa2c1d10f5fe15e7f73ed
navin017 <naveenelango018@gmail.com>
2023-03-15 16:03:06 +0530

    script

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 
-let a = prompt("text") ;
-console.log(a[2]);
+// let i= prompt("text") ;
+// console.log(i[2]);
+
+// let b = 3;
+// b >>= 2;
+// console.log(b);
+
+// let c = 4;
+// let a= 9;
+// a&&=4
+// c &&=2;
+// console.log(c);
+// console.log(a);
+
+// //switch
+
+let n= prompt("Name");
+v = n.toLocaleLowerCase()
+switch(v){
+    case "arokia":
+        console.log("roll no 1");
+        break;
+    case "ramani":
+        console.log("Roll no 2");
+        break;
+    case "naveen":
+        console.log("Roll no 3");
+        break;
+    default:
+        console.log("Not found");
+    
+}
 
 
-
-
+let p = prompt("பெயர்")
+if(p=="samy")
+console.log("done");
+else if(p=="ramani")
+    console.log("ya ya");
+    if(p=="alan")
+    console.log("put ur name");
+    if(p=="arokiasamy")
+    console.log("the robot");
+    if(p=="ramani")
+    console.log("not found");
 
